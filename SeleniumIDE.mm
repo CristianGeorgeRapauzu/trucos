51b5192612f225c0b3204cfeee080c16a307fb44
Cristian George Rapauzu <cristian.rapauzu@gmail.com>
2018-05-26 01:38:38 -0400

    Selenium IDE mind map as TRUC pattern

---
+++ SeleniumIDE.mm
@@ -0,0 +1,86 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1329423025146" ID="ID_28766640" LINK="http://seleniumhq.org/docs/02_selenium_ide.html" MODIFIED="1329425242532" TEXT="Selenium IDE">
+<node CREATED="1329423061615" ID="ID_1420659936" MODIFIED="1329423140101" POSITION="left" TEXT="alternative name"/>
+<node CREATED="1329423071147" ID="ID_1386980198" MODIFIED="1329423074428" POSITION="left" TEXT="dependencies">
+<node CREATED="1333081110722" ID="_" MODIFIED="1333081157460" STYLE="bubble" TEXT="Firefox"/>
+</node>
+<node CREATED="1329423077162" ID="ID_1744804825" MODIFIED="1329423412841" POSITION="left" TEXT="summary">
+<node CREATED="1329423242634" HGAP="19" ID="ID_1517966611" MODIFIED="1329425076529" STYLE="bubble" VSHIFT="7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><strong>Selenium</strong></b>: suite of tools to automate web browsers across many platforms.
+    </p>
+    <p>
+      <b>Selenium IDE</b>: Firefox add-on that will do simple record-and-playback of interactions with the browser
+    </p>
+    <p>
+      1. record interaction as a script
+    </p>
+    <p>
+      2. edit the script directly OR by right-clicking on the web interface under test
+    </p>
+    <p>
+      3. execute the script: full playback OR step by step OR up to breakpoint
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1329425467943" ID="ID_1939303600" MODIFIED="1329425775886" STYLE="bubble" TEXT="Selenium commands - &quot;Selenese&quot; - used to test:&#xa;- the existence of UI elements based on their HTML tags or specific content&#xa;- broken links, input fields, selection list options, submitting forms, and table data &#xa;- window size, mouse position&#xa;- alerts, pop up windows&#xa;- Ajax functionality, event handling"/>
+</node>
+<node CREATED="1329423086709" ID="ID_1015618048" MODIFIED="1329423088288" POSITION="left" TEXT="role">
+<node CREATED="1329423822786" HGAP="41" ID="ID_1909652730" MODIFIED="1329426614871" STYLE="bubble" TEXT="Automate browsers" VSHIFT="6">
+<node CREATED="1329423714065" ID="ID_1033219297" MODIFIED="1329423984149" TEXT="Web acceptance testing"/>
+<node CREATED="1329423847396" ID="ID_1580275581" MODIFIED="1329423984149" TEXT="Web based administration"/>
+</node>
+</node>
+<node CREATED="1329423090694" ID="ID_1119479833" MODIFIED="1329423163117" POSITION="left" TEXT="applicability">
+<node CREATED="1333081439641" ID="Freemind_Link_86724092" MODIFIED="1333081451000" STYLE="bubble" TEXT="Demo"/>
+<node CREATED="1333081428400" ID="Freemind_Link_262200344" MODIFIED="1333081436587" STYLE="bubble" TEXT="Smoke tests"/>
+<node CREATED="1329423264713" ID="ID_827035912" MODIFIED="1329424023790" STYLE="bubble" VSHIFT="6">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - create quick bug reproduction scripts
+    </p>
+    <p>
+      - create scripts to aid in automation-aided exploratory testing
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1329426490431" ID="ID_1742894634" MODIFIED="1329426593933" STYLE="bubble" TEXT="Rapid prototyping tool for building test scripts. &#xa;The Selenium developers recommend for serious, robust test automation &#xa;Selenium 2 to be used with one of the many supported programming languages."/>
+</node>
+<node CREATED="1329423096600" ID="ID_1853021387" MODIFIED="1329423099100" POSITION="left" TEXT="benefits">
+<node CREATED="1333081296549" ID="Freemind_Link_1288223283" MODIFIED="1333081360853" STYLE="bubble" TEXT="Client/Product Owner could specify Acceptence Tests before the UI exists."/>
+</node>
+<node CREATED="1329423102366" ID="ID_1418431422" MODIFIED="1329423104225" POSITION="right" TEXT="examples">
+<node CREATED="1333082731837" ID="Freemind_Link_12917478" LINK="http://elementalselenium.com/tips" MODIFIED="1527313056576" STYLE="bubble" TEXT="Selenium tips"/>
+<node CREATED="1333084000652" ID="Freemind_Link_1303742148" LINK="http://jroller.com/selenium/" MODIFIED="1333084022844" STYLE="bubble" TEXT="Selenium tutorial"/>
+<node CREATED="1333085177032" ID="Freemind_Link_1020925801" MODIFIED="1333085184003" TEXT="videos">
+<node CREATED="1333085187380" ID="Freemind_Link_543876489" LINK="http://www.youtube.com/watch?v=DFPacPVlUHk" MODIFIED="1333085208610" STYLE="bubble" TEXT="Selenium IDE presentation"/>
+<node CREATED="1333084967373" ID="Freemind_Link_1665731731" LINK="http://www.youtube.com/watch?v=eOEyvycPauw" MODIFIED="1333085226236" STYLE="bubble" TEXT="Selenium testing sample"/>
+</node>
+</node>
+<node CREATED="1329423111288" ID="ID_1130574723" MODIFIED="1329423183539" POSITION="right" TEXT="common confusions"/>
+<node CREATED="1329423106241" ID="ID_934585637" MODIFIED="1329423184977" POSITION="right" TEXT="pitfalls"/>
+<node CREATED="1329423124804" ID="ID_701634640" MODIFIED="1329423174836" POSITION="right" TEXT="tests of understanding"/>
+<node CREATED="1329423132163" ID="ID_1763484790" MODIFIED="1329423134273" POSITION="right" TEXT="see also">
+<node CREATED="1329426225317" ID="ID_1536979151" LINK="http://seleniumhq.org/docs/" MODIFIED="1329426268927" STYLE="bubble" TEXT="Selenium Documentation">
+<node CREATED="1329425887701" ID="ID_354295597" LINK="http://release.seleniumhq.org/selenium-core/1.0.1/reference.html" MODIFIED="1329426268927" STYLE="bubble" TEXT="Selenium Reference"/>
+<node CREATED="1329427072130" ID="ID_1471065104" LINK="http://seleniumhq.org/docs/06_test_design_considerations.html" MODIFIED="1329427106068" TEXT="Test Design Considerations"/>
+</node>
+<node CREATED="1329426068626" ID="ID_744424920" LINK="http://groups.google.com/group/selenium-users" MODIFIED="1329426268927" STYLE="bubble" TEXT="Selenium User Group"/>
+<node CREATED="1329493496499" ID="ID_1500326606" LINK="http://refcardz.dzone.com/refcardz/getting-started-selenium-20" MODIFIED="1329493521077" STYLE="bubble" TEXT="Selenium 2.0 - DZone Refcardz"/>
+<node CREATED="1329514639621" ID="ID_1066619208" LINK="http://java.dzone.com/articles/roo-selenium-webdriverfailsafe" MODIFIED="1329514942690" STYLE="bubble" TEXT="Roo,Selenium WebDriver, JUnit, Maven Failsafe"/>
+<node CREATED="1333084494061" ID="Freemind_Link_1518133302" LINK="http://www.qaautomation.net/?p=263" MODIFIED="1333084530076" STYLE="bubble" TEXT="Selenium 2 example"/>
+</node>
+</node>
+</map>
